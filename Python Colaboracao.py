1617a2225188b169e7c5503bbcc5fb713cdc17f4
Priscila Nascimento Paulino <priscilanascimentopaulino@gmail.com>
2025-07-09 13:31:26 -0300

    Add files via upload
    Arquivo para edição na linguagem Python

---
+++ Python Colaboracao.py
@@ -0,0 +1,12 @@
+print("=== Bem-vindo ao Restaurante===")
+
+nome = input("Por favor, informe seu nome: ")
+quantidade_pessoas = input("Quantas pessoas estarão na reserva? ")
+data_reserva = input("Para qual data deseja reservar? (ex: 08/07/2025): ")
+horario_reserva = input("Qual horário deseja reservar? (ex: 19:30): ")
+
+print(f"Nome: {nome}")
+print(f"Número de pessoas: {quantidade_pessoas}")
+print(f"Data: {data_reserva}")
+print(f"Horário: {horario_reserva}")
+print("\n.")
